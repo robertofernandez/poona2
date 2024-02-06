800bb38741b53b69fcbd3aa8cdb8507c34f0c3b5
Roberto G. Fernández <rgfernan@gmail.com>
2024-02-06 02:46:33 -0300

    Multiple input controllers to enable different players #7 * Added rotation control to wheels.

--- Poona2D/Assets/Scripts/RobotZeroWheel.cs
+++ Poona2D/Assets/Scripts/RobotZeroWheel.cs
@@ -5,12 +5,17 @@ public class RobotZeroWheel : MonoBehaviour {
 	public Transform characterTransform;
     public float currentRotation = 0;
 
+    public GameObject inputControllerObject;
+
+    private IInputController inputController;
+
 	void Start () {
         transform.rotation =  Quaternion.Euler(0, 0, currentRotation);
+        inputController = inputControllerObject.GetComponent<IInputController>();
 	}
 	
 	void Update () {
-		float xInput = Input.GetAxis("Horizontal");
+		float xInput = inputController.GetXInput();
         if (xInput > 0) {
             currentRotation--;
         } else if (xInput < 0) {
